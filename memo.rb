9509c26af49c2cb82cdb2614dafe7dbdb49e5ad6
shogoyamaoe <shogo.1227@mineo.jp>
2021-12-12 12:46:31 +0900

    メモ帳提出２回目

--- memo.rb
+++ memo.rb
@@ -5,45 +5,45 @@ puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 memo_type = gets.to_i
 
 if memo_type == 1
-puts "1が選択されました"
-puts "拡張子を除いたファイル名を入力してください"
+  puts "1が選択されました"
+  puts "拡張子を除いたファイル名を入力してください"
     
-file_name = gets.chomp
-CSV.open("#{file_name}.csv", "w") do |csv|
+  file_name = gets.chomp
+  CSV.open("#{file_name}.csv", "w") do |csv|
 
-puts "文字を入力してください"
-puts "入力完了後 control + D を押してください"
+  puts "文字を入力してください"
+  puts "入力完了後 control + D を押してください"
 
-contents = readlines
-elementNumber = contents.length
-a = 0
-while a < elementNumber
-contents[a] = contents[a].chomp
-a +=1
-end
-csv <<  contents
+  contents = readlines
+  elementNumber = contents.length
+  a = 0
+  while a < elementNumber
+    contents[a] = contents[a].chomp
+    a +=1
+  end
+  csv <<  contents
 end
 
 elsif memo_type == 2
-puts "2が選択されました"
-puts "拡張子を除いたファイル名を入力してください"
+  puts "2が選択されました"
+  puts "拡張子を除いたファイル名を入力してください"
 
-file_name = gets.chomp
-CSV.open("#{file_name}.csv", "a") do |csv|
+  file_name = gets.chomp
+  CSV.open("#{file_name}.csv", "a") do |csv|
 
-puts "文字を入力してください"
-puts "入力完了後 control + D を押してください"
+  puts "文字を入力してください"
+  puts "入力完了後 control + D を押してください"
 
-contents = readlines
-elementNumber = contents.length
-a = 0
-while a < elementNumber
-contents[a] = contents[a].chomp
-a +=1
-end
-csv <<  contents
+  contents = readlines
+  elementNumber = contents.length
+  a = 0
+  while a < elementNumber
+    contents[a] = contents[a].chomp
+    a +=1
+  end
+  csv <<  contents
 end
 
 else 
-puts "もう一度選択し直してください"
+  puts "もう一度選択し直してください"
 end
